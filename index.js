0ef6897e46e364b6afac1235c30dee03182bd24d
Andrew Graf <podeye@hotmail.com>
2021-04-21 10:21:31 -0600

    server

---
+++ index.js
@@ -0,0 +1,27 @@
+const {ApolloServer, PubSub} = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const {MONGODB} = require('./config')
+
+// String! = param must be a string !=must return
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs:typeDefs,
+  resolvers:resolvers,
+  context:({req})=>({req, pubsub})
+})
+
+const PORT = process.env.port || 5000;
+
+mongoose.connect(MONGODB, {useNewUrlParser:true, useUnifiedTopology: true}).then(()=>{
+  console.log("Mongo connected")
+  return server.listen({port:PORT})
+}).then(res=>{
+  console.log(`Server started at ${res.url}`)
+}).catch(err=>{
+  console.error(err);
+})
